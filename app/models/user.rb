5fac81cec73cb3d807181348190baa8dfb9bbd48
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-10-15 20:23:52 +0900

    バリデーション 修正＆itemsコントローロー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :email,           uniquness:true, format: { with: /@/}
+         validates :email,           format: { with: /@/}
          validates :password,        length: { minimum: 6 }, confirmation: true,  format: { with: /\A[a-zA-Z0-9]+\z/ }
          validates :nickname,        presence: true
-         validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, /\A[ァ-ヶー－]+\z/}
-         validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, /\A[ァ-ヶー－]+\z/}
+         validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+         validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
          validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
          validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
          validates :birthday,        presence: true
