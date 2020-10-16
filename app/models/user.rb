f2b23cad3dfbfc7514e7f430a305af89a9536d63
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-10-16 15:45:26 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,13 @@ class User < ApplicationRecord
 
          validates :email,           format: { with: /@/}
          validates :password,        length: { minimum: 6 }, confirmation: true,  format: { with: /\A[a-zA-Z0-9]+\z/ }
-         validates :nickname,        presence: true
-         validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-         validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-         validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-         validates :birthday,        presence: true
+    with_options presence: true do      
+         validates :nickname,        
+         validates :first_name,       format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+         validates :last_name,        format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+         validates :first_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :last_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :birthday       
+    end
 
 end
