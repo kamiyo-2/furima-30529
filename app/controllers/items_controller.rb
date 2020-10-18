064578cbca91381e092659d49bc30d14ca6e1f1e
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-10-18 17:11:54 +0900

    item.controller修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,7 +31,7 @@ class ItemsController < ApplicationController
 
    def update
      if @item.update(item_params)
-       redirect_to item_path(item.id), method: :get
+       redirect_to item_path(@item.id), method: :get
      else
        render :edit
      end
