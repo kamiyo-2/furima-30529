8e5a5cfb4bc9ec415a492f9885baa2ce86b0ccae
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-10-18 11:39:33 +0900

    itm.controller修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,26 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    if @item.user != current_user
+      redirect_to root_path
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path(item.id), method: :get
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
