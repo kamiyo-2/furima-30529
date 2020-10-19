036ec382512c2dba29fd6f0629ed602fbbb81aa6
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-10-19 23:06:43 +0900

    order.index.html.erb設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
 
   def index
     @order_street_address = OrderStreetAddress.new
+    @item = Item.find(params[:item_id])
   end
 
  
@@ -18,7 +19,7 @@ class OrdersController < ApplicationController
  private
 
  def order_street_address_params
-  params.require(:order_street_address).permit(:post_code, :area_id, :city, :address, :building_name, :phone_number, :order, :user, :item)
+  params.require(:order_street_address).permit(:post_code, :area_id, :city, :address, :building_name, :phone_number, :order).merge(user_id: current_user.id, item_id: params[:item_id])
  end
  
 end
